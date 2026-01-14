7255b11123f2d71ab6b0f1f42f4aaefc49f58c1a
sisoia ankush <techankush2021@gmail.com>
2026-01-14 22:20:27 +0530

    lecture 1 of js

---
+++ lect1.js
@@ -0,0 +1,196 @@
+let a= 14;
+console.log(typeof a);//number
+let b= -8.9;
+console.log(typeof b);//number
+
+let a=10;
+let b= 5;
+console.log(a+b); 
+
+let x=5;
+let y= "5";
+console.log(x+y);//55
+//how to fix it
+console.log(x+Number(y));
+
+/*let a= "42";
+console.log(typeof a);
+console.log(Number(a)+1);
+
+
+
+let a= 10;
+let b= 5;
+console.log(a+b);//15
+console.log(a-b);//5
+console.log(a*b);//50
+console.log(a/b);///2
+console.log(a%b);//0
+console.log(a**b);
+
+// NaN
+/*console.log("helllo"*2);
+console.log(0/0);
+console.log("hello"/2);
+console.log(NaN-1);*/
+
+/*Operator precedence:
+/*console.log(2+3-2);//3
+console.log(2-3+1);//0
+console.log(2**3+3-4/2);//9
+let a=10;
+let b= 5;
+let f= a=b+2;
+console.log(f);
+
+let c= 40;
+let d= 50;
+let e= c=d=c;
+console.log(e)
+
+console.log(true || false && flase);*/
+
+
+//let keyword
+
+/*let a= 30;
+console.log(a+1);
+let side =4;
+console.log(side*side);
+
+{
+    let x= 4;
+    console.log(x)
+}
+console.log(x)*/
+
+/*let name= "ankush";
+console.log(name);
+name ="sisodia";
+console.log(name);
+name="ayush";
+console.log(name);*/
+
+/*let num1= 4;
+let num2=4;
+let sum = num1+num2;
+console.log(sum);*/
+
+//const keyword
+
+/*const pie =3.14;
+console.log(pie);
+pie=5;
+console.log(pie);*/
+
+//assignment operator
+
+/*let a= 5;
+let b=a;
+console.log(b);
+
+let c=3;
+c+=1;
+console.log(c);
+
+let e=10;
+e-=4;
+e*=2;
+console.log(e);*/
+
+/*let p= 2;
+p**=3;
+console.log(p);
+
+let a= 5;
+let b=a++;
+a+=2;
+console.log(a);
+console.log(b);
+
+let text ="hello";
+text="world";
+console.log(text);
+
+let x=5;
+let y = (x+2)+3;
+console.log(x,y);*/
+
+//uniary opearor
+
+/*let age =22;
+let age1=age++
+console.log(age1,age)//22 23
+
+let a= 22;
+let a1= ++a;
+console.log(a1,a);//23 23
+
+let num=5;
+let newNum =num++;
+newNum =++num;
+console.log(newNum,num); */
+
+//boolean
+
+/*let age =23;
+let isadult=true;
+
+age=17;
+isadult=false;
+console.log(isadult)
+
+let a= 2;
+let b=5;
+console.log(a>b);
+
+console.log(+true);
+console.log(+false);*/
+
+/*let a= 5;
+console.log(a);
+a= "true";
+console.log(typeof a);
+
+let marks = 76;
+let ispass =true;
+console.log(ispass);*/
+
+//string
+/*let role="ironman";
+let char ="b";
+let num="5";
+console.log(typeof char);
+console.log(typeof role);
+console.log(typeof num);
+
+let sent= 'this is"apple" ';
+let sent1="this is 'apple' ";
+console.log(sent);
+console.log(sent1);*/
+
+/*let name ="TONYSTARK";
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[4]);
+console.log(name[6]);
+console.log(name.length);
+
+let role="bat man";
+console.log(role.length);
+console.log("".length);
+ console.log("tony"+ " "+"stark");
+ let istname="ankush";
+ let lastname ="sisodia";
+ console.log(istname+" " +lastname);*/
+
+ //question
+/* let name="ankush";
+ console.log(typeof name);
+ console.log(name.length);
+
+let firstname ="ankush";
+console.log(typeof firstname);
+console.log(firstname[0]);
+console.log(firstname[firstname.length-1]);*/
+
