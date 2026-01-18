fde249db532b43475634701c95eb7f71aea92032
sisoia ankush <techankush2021@gmail.com>
2026-01-18 23:15:10 +0530

    lecture 5 of js

---
+++ lect5.js
@@ -0,0 +1,142 @@
+//object literal
+/*let student ={
+    name:"ankush",
+    marks:67,
+    age:66,
+};
+console.log(student);
+console.log( typeof student);
+let students1= ["ankush",67,66];
+console.log( typeof students1);*/
+/*const boys ={
+    name :"ayush",
+    age:22,
+    marks:45,
+};
+console.log(boys);
+console.log(boys.name);
+console.log(boys.age);*/
+/*const items ={
+    price :566.9,
+    discount :99.99,
+    color:["red","orange"],
+};
+console.log(typeof items);
+console.log(items.color);*/
+//create a post of twitter 
+/*const post ={
+    username :"@ayushkumar",
+    content:"cricketpage",
+    likes :22.6,
+    repost:4,
+    tag:["@aman","@aryan"],
+};
+console.log(post);
+console.log(typeof post);*/
+/*const obj ={
+    1:"a",
+    2:"b",
+    3:"c",
+};
+console.log(obj)*/
+/*let person= {
+    name:"manoj",
+    age:66,
+};
+console.log(person);
+console.log(person.city ="delhi");
+console.log(person.name="arjun");
+console.log(person.marks =[99,98,97]);
+console.log(delete person.delhi)*/
+/*const boys ={
+  name :"ayush",
+  marks :{
+    science :98,
+    maths :97,
+  }
+};
+console.log(boys);*/
+/*const boys ={
+    ankush :{
+age:22,
+grade:"a"
+    },
+    ayush :{
+        age:21,
+        grade:"b",
+    }
+};
+console.log(boys)
+console.log(boys.ayush.grade);
+console.log(boys.ayush.age);
+console.log(boys.ayush.city ="delhi");*/
+//method inside object
+/*let car ={
+    brand:"tela",
+    model:"model s",
+    start() {
+        console.log("car started ");
+    }
+};
+console.log(car.start());*/
+// array of object
+/*const classinfo= [
+    {
+name:"ankush",
+age:22,
+city:"delhi",
+    },
+    {
+    name:"ayush",
+    age:21,
+    city :"pune",
+    },
+];
+*/
+//maths object
+/*console.log(Math.pow(2,4));
+console.log(Math.floor(5.333));
+console.log(Math.floor(5.8));
+console.log(Math.ceil(5.333));
+console.log(Math.floor(5.333));
+console.log(Math.min(1,3,4,5,7,9));
+console.log(Math.max(1,3,4,5,7,9));
+console.log(Math.abs(-8));*/
+/*console.log("random  number between 0-1")
+let num = Math.random();
+console.log(num);
+console.log("by multiplying by 10 to get between 0 to 10 not 10")
+let num1 = num*10;
+console.log(num1);
+console.log("remove decimal number");
+let num2= Math.floor(num1);
+console.log(num2);
+let num3= num2-4;
+console.log(num3);*/
+//console.log(Math.floor(Math.random()*6)+1);//0 1 2 3 4 5 6 not 7
+//console.log(Math.floor(Math.random()*5)+21);// 
+//console.log(Math.floor(Math.random()*6)+1);
+/*csole.log("skip the game");
+        break;
+    }else if(guess==random){
+        console.log("you won");
+    }else {
+        guess=prompt("wrong guess");
+    }
+}*/
+//dice number
+//console.log(Math.floor(Math.random()*6)+1);
+/*const car ={
+    name:"suzuki",
+    model :"x",
+    color:"black",
+};
+console.log(car["name"]);*/
+let person ={
+    name :"ajauy",
+    age:45,
+    city :"delhi",
+};
+console.log(person);
+console.log(person.city="newyork");
+console.log(person.country="usa");
